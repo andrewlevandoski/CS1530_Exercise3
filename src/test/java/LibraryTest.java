c7b7d1bcb058e33a90044520a79c22c7b1a5c37e
Andrew Levandoski <agl25@pitt.edu>
2017-10-23 13:53:06 -0400

    adds test

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.*;
  */
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        assertEquals(15, Main.triangle(5));
     }
 }
